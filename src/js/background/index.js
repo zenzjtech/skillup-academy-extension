4f82f69ab83066ae03450851ebbbf9f5ca2152d0
Nguyen Canh Toan <canhtoannguyen60@gmail.com>
2021-02-15 15:46:24 +0700

    handle cors

--- src/js/background/index.js
+++ src/js/background/index.js
@@ -4,6 +4,40 @@ require('chrome-extension-async')
 
 import { process} from "./facebook-id";
 
+chrome.webRequest.onBeforeSendHeaders.addListener/**
+ *
+ */
+(function(requestHeaders) {
+  requestHeaders = requestHeaders.requestHeaders;
+  
+  let temp = requestHeaders.findIndex(function(d) {
+    return "Origin" === d.name
+  });
+  if (temp === -1)
+    requestHeaders.push({
+      name: "Origin",
+      value: "https://www.facebook.com"
+    })
+  else
+    requestHeaders[temp].value = "https://www.facebook.com";
+  
+  temp = requestHeaders.findIndex(function(d) {
+    return "referer" === d.name
+  });
+  if (temp === -1)
+    requestHeaders.push({
+      name: "Referer",
+      value: "https://www.facebook.com"
+    })
+  else
+    requestHeaders[temp].value = "https://www.facebook.com";
+  return {
+    requestHeaders
+  }
+}, {
+  urls: ["https://*.facebook.com/*"]
+}, ["blocking", "requestHeaders", "extraHeaders"]);
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   (async () => {
     const { url }  = message;
